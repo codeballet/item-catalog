8e881a741dda61ea13d73c1603fc39495112c8b3
johan <stjernholm@msn.com>
2019-06-02 22:45:03 +0800

    Fix: models.py conform to pycodestyle

--- models.py
+++ models.py
@@ -4,15 +4,21 @@ from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
 from passlib.apps import custom_app_context as pwd_context
-import random, string
-from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+import random
+import string
+from itsdangerous import (TimedJSONWebSignatureSerializer as
+                          Serializer,
+                          BadSignature,
+                          SignatureExpired)
 
 
 Base = declarative_base()
 
 
 # Generate a secret key to create and verify tokens
-secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+secret_key = ''.join(
+    random.choice(string.ascii_uppercase + string.digits)
+    for x in xrange(32))
 
 
 class User(Base):
@@ -41,8 +47,8 @@ class User(Base):
         return pwd_context.verify(password, self.password_hash)
 
     # Method to generate auth tokens
-    def generate_auth_token(self, expiration = 600):
-        s = Serializer(secret_key, expires_in = expiration)
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
         return s.dumps({"user_id": self.user_id})
 
     # Method to verify auth tokens
@@ -59,18 +65,17 @@ class User(Base):
             return None
         user_id = data['user_id']
         return user_id
-        
-
-
 
 
 class Category(Base):
     __tablename__ = 'category'
-   
+
     category_id = Column(Integer, primary_key=True)
     category_name = Column(String(80), nullable=False)
     user_id = Column(Integer, ForeignKey('user.user_id'), nullable=False)
-    user = relationship('User', backref=backref('categories', cascade='all, delete-orphan'))
+    user = relationship('User', backref=backref('categories',
+                                                cascade='all,
+                                                delete-orphan'))
 
     @property
     def serialize(self):
@@ -82,37 +87,33 @@ class Category(Base):
         }
 
 
-
-
-
 class Item(Base):
     __tablename__ = 'item'
 
-
-    item_id = Column(Integer, primary_key = True)
-    item_name =Column(String(80), nullable = False)
+    item_id = Column(Integer, primary_key=True)
+    item_name = Column(String(80), nullable=False)
     item_description = Column(String)
     item_price = Column(String(20))
     item_date = Column(DateTime, default=func.now())
     category_id = Column(Integer, ForeignKey('category.category_id'))
-    category = relationship('Category', backref=backref('items', cascade='all, delete-orphan'))
-
+    category = relationship('Category', backref=backref('items',
+                                                        cascade='all,
+                                                        delete-orphan'))
 
     @property
     def serialize(self):
-       """Return object data in easily serializeable format"""
-       return {
+        """Return object data in easily serializeable format"""
+        return {
            'item_name': self.item_name,
            'item_description': self.item_description,
            'item_id': self.item_id,
            'item_price': self.item_price,
            'item_date': self.item_date,
            'category_id': self.category_id
-       }
-
+        }
 
 
 engine = create_engine('sqlite:///catalog.db')
- 
+
 
 Base.metadata.create_all(engine)
